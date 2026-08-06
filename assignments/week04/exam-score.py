bcdfc9af02dc939e75e5b33c04996e7f20f40957
SittaPajaisuk <sitta.pa@ku.th>
2026-08-06 19:37:07 +0700

    write exam-score.py (finished)

---
+++ assignments/week04/exam-score.py
@@ -0,0 +1,12 @@
+score_list = []
+for i in range(5):
+    score = int(input(f"Enter score of student {i+1}: "))
+    score_list.append(score)
+
+print("")
+
+for s in range(len(score_list)):
+    if score_list[s] >= 50:
+        print(f"Student {s + 1}: {score_list[s]} -> ผ่าน")
+    else:
+        print(f"Student {s + 1}: {score_list[s]} -> ไม่ผ่าน")
