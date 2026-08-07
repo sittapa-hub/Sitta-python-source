35c32752b364e5ceb05201046343464a1ab2cb5c
Sitta Pajaisuk <sitta.pa@ku.th>
2026-08-07 15:24:10 +0700

    write function calculate circle area

--- labs/week06-lab/example/geometry.py
+++ labs/week06-lab/example/geometry.py
@@ -1,5 +1,5 @@
 def calculate_triangle_area(height, base):
-    """Calculates and displays rectangle area"""
+    """Calculates and displays triangle area"""
     area = 0.5 * base * height
     print(f"Triangle with height {height} and base {base}")
     print(f"Area = 1/2 x {height} × {base} = {area}")
@@ -9,3 +9,15 @@ print("Calculating triangle areas:")
 calculate_triangle_area(5, 3)
 calculate_triangle_area(10, 7)
 
+
+ # จากตัวอย่าง ให้สร้าง function สำหรับคำนวณพท.วงกลม
+def calculate_circle_area(radius):
+    """Calculates and displays circle area"""
+    area = 3.14 * radius ** 2
+    print(f"Circle with radius {radius}")
+    print(f"Area = 3.14 x {radius} x {radius} = {area}")
+    print()
+
+print("Calculating circle areas:")
+calculate_circle_area(5)
+calculate_circle_area(7)
